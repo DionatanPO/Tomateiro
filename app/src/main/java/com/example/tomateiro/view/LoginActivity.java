20d0c652e7a2d3a273587413019aaec442209abc
Dionatan <dionatanoliveirapc@gmail.com>
2020-11-28 11:24:48 -0300

    Desenvolvimento v1.01

--- app/src/main/java/com/example/tomateiro/view/LoginActivity.java
+++ app/src/main/java/com/example/tomateiro/view/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.example.tomateiro.view;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,8 @@ import com.example.tomateiro.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button btn_registrar, getBtn_entrar;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,24 @@ public class LoginActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_login);
 
+        context = this;
+
+        btn_registrar = findViewById(R.id.btn_registrar);
+        getBtn_entrar = findViewById(R.id.btn_entrar);
+
+        btn_registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, RegistroActivity.class);
+                startActivity(intent);
+            }
+        });
+        getBtn_entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, PainelActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
